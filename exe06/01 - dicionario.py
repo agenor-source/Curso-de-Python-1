d8fe654ca6d3be64756b3c6f0b5c0bfdb03be54e
agenor-source <agenor@musica.ufrj.br>
2026-08-24 21:38:39 -0300

    x2

---
+++ exe06/01 - dicionario.py
@@ -0,0 +1,22 @@
+produtos = [ 
+    { 
+        "nome": "Notebook",
+        "preco": 4500
+    }
+    {
+    "nome": "celular",
+            "preco": 1800.50
+    },
+
+    { "nome": "Mouse",
+        "preco": 45},
+
+    {"nome": "Monitor",
+            "preco": 670
+    },
+    {"nome": "Teclado",
+            "preco": 67
+
+    },
+]
+#somar todos os valores dos produtos.
